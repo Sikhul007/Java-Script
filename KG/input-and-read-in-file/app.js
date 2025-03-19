8c7dc0d2008e7445445cedf1a6d12f301f01183a
MD. Sikhul Islam Shihab <123489513+Sikhul007@users.noreply.github.com>
2025-03-19 07:25:13 +0600

    Delete KG directory

--- KG/input-and-read-in-file/app.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const reqHandler = require("./index");
-const server = http.createServer(reqHandler);
-const port = 3001;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
